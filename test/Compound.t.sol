322719e51cd6253139567efbe410fed5ca6ea26d
roy_chen <roy_chen@gss.com.tw>
2023-06-11 19:47:13 +0800

    完成 Q3 test

--- test/Compound.t.sol
+++ test/Compound.t.sol
@@ -40,6 +40,7 @@ contract CompoundTest is Test {
     }
     RateInfo public _rateInfoA;
     RateInfo public _rateInfoB;
+    SimplePriceOracle public _priceOracle;
 
     /// ===== User =====
     address public _admin;
@@ -54,6 +55,7 @@ contract CompoundTest is Test {
         vm.startPrank(_admin);
 
         _comptroller = new Comptroller();
+        _priceOracle = new SimplePriceOracle();
         _decimals = 18;
         _underlyingA = create_ERC20("LL", "LL", _decimals);
         _rateInfoA = RateInfo(0, 0);
@@ -63,9 +65,15 @@ contract CompoundTest is Test {
             _rateInfoA,
             1e18,
             _decimals,
-            "0x0"
+            "0x0",
+            _priceOracle
         );
-        _comptroller._supportMarket(CToken(address(_cTokenA)));
+        require(_comptroller._supportMarket(CToken(address(_cTokenA))) == 0, "add cTokenA to market, got error");
+        // CToken[] memory tokens = new CToken[](1);
+        // tokens[0] = CToken(address(_cTokenA));
+        // uint[] memory borrowCaps = new uint[](1);
+        // borrowCaps[0] = 0;
+        // _comptroller._setMarketBorrowCaps(tokens, borrowCaps);  // 設定 TokenA 無借出上限
         vm.stopPrank();
     }
 
@@ -101,17 +109,115 @@ contract CompoundTest is Test {
 
     /**
      * 3. 讓 User1 borrow/repay
-     *    - [ ] 部署第二份 cERC20 合約，以下稱它們的 underlying tokens 為 token A 與 token B。
-     *    - [ ] 在 Oracle 中設定一顆 token A 的價格為 $1，一顆 token B 的價格為 $100
-     *    - [ ] Token B 的 collateral factor 為 50%
-     *    - [ ] User1 使用 1 顆 token B 來 mint cToken
-     *    - [ ] User1 使用 token B 作為抵押品來借出 50 顆 token A
+     *    - [x] 部署第二份 cERC20 合約，以下稱它們的 underlying tokens 為 token A 與 token B
+     *    - [x] 在 Oracle 中設定一顆 token A 的價格為 $1，一顆 token B 的價格為 $100
+     *    - [x] Token B 的 collateral factor(抵押品能借出幾成價值) 為 50%
+     *    - [x] User1 使用 1 顆 token B 來 mint cToken
+     *    - [x] User1 使用 token B 作為抵押品來借出 50 顆 token A
      */
-    // function testBorrowAndRepay() public {
-    //     vm.startPrank(_user1);
-    //     // deploy cTokenB
-    //     vm.stopPrank();
-    // }
+    function testBorrow() public {
+      borrowEnvSetUp();
+    }
+
+    function borrowEnvSetUp() public {
+        uint priceTokenA = 1;
+        uint priceTokenB = 100;
+        uint depositTokenB_amount = 1 * 10 ** _decimals;
+        uint borrowTokenA_amount = 50 * 10 ** _decimals;
+
+        vm.startPrank(_admin);
+        // - [x] 部署第二份 cERC20 合約，以下稱它們的 underlying tokens 為 token A 與 token B
+        // 1. deploy tokenB & cTokenB
+        _underlyingB = create_ERC20("MM", "MM", _decimals);
+        _rateInfoB = RateInfo(0, 0);
+        _cTokenB = create_cERC20(
+            _comptroller,
+            _underlyingB,
+            _rateInfoB,
+            1e18,
+            _decimals,
+            "0x0",
+            _priceOracle
+        );
+
+        // 2. list cTokenB to market
+        uint addMarketResponse = _comptroller._supportMarket(CToken(address(_cTokenB)));
+        assertEq(addMarketResponse, 0);
+
+        // - [x] 在 Oracle 中設定一顆 token A 的價格為 $1，一顆 token B 的價格為 $100
+        _priceOracle.setUnderlyingPrice(CToken(address(_cTokenA)), priceTokenA);
+        _priceOracle.setUnderlyingPrice(CToken(address(_cTokenB)), priceTokenB);
+        assertEq(_priceOracle.getUnderlyingPrice(CToken(address(_cTokenA))), priceTokenA);
+        assertEq(_priceOracle.getUnderlyingPrice(CToken(address(_cTokenB))), priceTokenB);
+
+
+        // - [x] 設定 Token B 的 collateral factor(抵押品能借出幾成價值) 為 50%
+        // - 聽說在 solidity 的世界裡, 有乘除一起的話，一律先乘再除，避免有除不盡的奇怪問題
+        // - 別的同學有使用：5e16
+        uint setCollateralFactorResponse = _comptroller._setCollateralFactor(CToken(address(_cTokenB)), 50 * 10 ** _decimals / 100);
+        assertEq(setCollateralFactorResponse, 0);
+
+
+        // 放一些 token A 進去，準備給 user1 借
+        _underlyingA.mint(borrowTokenA_amount * 10);
+        _underlyingA.approve(address(_cTokenA), borrowTokenA_amount * 10);
+        _cTokenA.mint(borrowTokenA_amount * 10);
+        vm.stopPrank();
+
+
+        vm.startPrank(_user1);
+        // - [x] User1 使用 1 顆 token B 來 mint cToken
+        //  1. mint underlying token & approve for cToken
+        _underlyingB.mint(depositTokenB_amount);
+        _underlyingB.approve(address(_cTokenB), depositTokenB_amount);
+        //  2. mint cToken
+        uint user1HoldUnderlyingB_Amount = _underlyingB.balanceOf(_user1);
+        _cTokenB.mint(depositTokenB_amount);
+        assertEq(_underlyingB.balanceOf(_user1), user1HoldUnderlyingB_Amount - depositTokenB_amount);
+
+        // - [x] User1 使用 token B 作為抵押品來借出 50 顆 token A
+        //  1. user1 先將 token B enter market, 才算把 tokenB 當作抵押品
+        uint user1HoldUnderlyingA_Amount = _underlyingA.balanceOf(_user1);
+        address[] memory cTokens= new address[](1);
+        cTokens[0] = address(_cTokenB);
+        _comptroller.enterMarkets(cTokens);
+        //  2. user1 來去借錢
+        _cTokenA.borrow(borrowTokenA_amount);
+        assertEq(_underlyingA.balanceOf(_user1), user1HoldUnderlyingA_Amount + borrowTokenA_amount);
+        // CErc20.sol : function borrow(uint borrowAmount) external returns (uint)
+        //   CToken.sol : function borrowInternal(uint borrowAmount) internal nonReentrant
+        //   CToken.sol : function borrowFresh(address payable borrower, uint borrowAmount) internal
+        //     Comptroller.sol : function borrowAllowed(address cToken, address borrower, uint borrowAmount) override external returns (uint)
+        vm.stopPrank();
+    }
+
+    /** Comptroller.sol
+        user 能做的：
+        將該資產加入抵押品：function enterMarkets(address[] memory cTokens) override public returns (uint[] memory)
+
+        admin 能做的：
+        設定多少 %數能被清算：function _setCloseFactor(uint newCloseFactorMantissa) external returns (uint)
+        抵押品能借出幾成價值: function _setCollateralFactor(CToken cToken, uint newCollateralFactorMantissa) external returns (uint)
+        設定清算獎勵：function _setLiquidationIncentive(uint newLiquidationIncentiveMantissa) external returns (uint)
+        讓 cToken 上市場：function _supportMarket(CToken cToken) external returns (uint)
+        設定池子能被借出的上限：function _setMarketBorrowCaps(CToken[] calldata cTokens, uint[] calldata newBorrowCaps) external
+        function _setBorrowCapGuardian(address newBorrowCapGuardian) external
+     */
+
+    /**
+     * 4. 清算
+     *    - [ ] 延續 (3.) 的借貸場景，調整 token B 的 collateral factor，讓 User1 被 User2 清算
+     */
+
+
+    /**
+     * 5. 清算
+     *    - [ ] 延續 (3.) 的借貸場景，調整 oracle 中 token B 的價格，讓 User1 被 User2 清算
+     */
+
+
+
+
 
     //////////////////////////////////
     // helper functions
@@ -132,7 +238,8 @@ contract CompoundTest is Test {
         RateInfo memory rateInfo,
         uint initialExchangeRateMantissa,
         uint8 decimals,
-        bytes memory becomeImplementationData
+        bytes memory becomeImplementationData,
+        SimplePriceOracle priceOracle
     ) public returns (CErc20Delegator) {
         // depoly rate model & set rate
         WhitePaperInterestRateModel interestRateModel = new WhitePaperInterestRateModel(
@@ -163,11 +270,9 @@ contract CompoundTest is Test {
         // 但先搞作業ㄅ
         comptroller._become(unitroller);
 
-        // depoly price oracle
-        SimplePriceOracle priceOracle = new SimplePriceOracle();
         // set price oracle to comptroller
         // 注意：function 是要對 implementation contract 執行，不是對 proxy contract 執行
-        // 設定 Orace price
+        // 設定 Oracle price
         comptroller._setPriceOracle(priceOracle);
 
         return cERC20;
